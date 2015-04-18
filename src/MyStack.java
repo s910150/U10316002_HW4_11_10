e60ccc08f3b8c77c8b0639bc25e8269c68f85c13
s910150 <s9101505@gmail.com>
2015-04-18 23:51:47 +0800

    增加註解

--- src/MyStack.java
+++ src/MyStack.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 
 public class MyStack extends ArrayList{
 	
+  //new ArrayList
   private ArrayList<String> list = new ArrayList<String>();
 
+  //test if list is empty
   public boolean isEmpty() {
     return list.isEmpty();
   }
 
+  //get list's size
   public int getSize() {
     return list.size();
   }
@@ -16,12 +19,14 @@ public class MyStack extends ArrayList{
     return list.get(getSize() - 1);
   }
 
+  //first in last out
   public String pop() {
     String o = list.get(getSize() - 1);
     list.remove(getSize() - 1);
     return o;
   }
 
+  //add elements in array
   public void push(String o) {
     list.add(o);
   }
--- src/TestMyStack.java
+++ src/TestMyStack.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class TestMyStack {
 	public static void main(String[] args){
 		
+		//create an object
 		MyStack stack = new MyStack();
 		Scanner input = new Scanner(System.in);
 		
+		//prompt user to enter 5 strings
 		System.out.println("Please enter 5 Strings.");
+		
+		//put the strings in array in order
 		for(int i = 1;i<=5;i++){
 			stack.push(input.next());
 		}
 		
 		System.out.println("Inverse result:");
+		
+		//print the inverse order 
 		while(!stack.isEmpty()){
 			System.out.print(stack.pop()+" ");
 		} 
+		
+		input.close();
 		
 }//end main method
 }
